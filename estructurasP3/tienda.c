ec457ea2d8adbcd03591483288700d11afce21d7
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2024-02-27 22:11:38 -0600

    Se añade estructura básica de menú, struct producto y un archivo ejecutable.

--- estructurasP3/tienda.c
+++ estructurasP3/tienda.c
@@ -2,10 +2,54 @@
 struct producto{
     float precio;
     char nombre[50];
-    char tipo[60];
+    char tipo[5][20];
 };
+
+void llenarCarrito(){
+    /*Elige y escribe los productos a comprar*/
+    printf("LLenar carrito");
+}
+void listarProductos(){
+    /*Lista los productos a comprar*/
+    printf("Listar productos");
+}
+void devolverProducto(){
+    printf("Devolver carrito");
+}
+void pagarCarrito(){
+    printf("Pagar carrito");
+}
+
 int main()
 {
-    
+    int opcion;
+    while(opcion != 5)
+    {
+        printf("\n\tTienda de productos en venta\n\n");
+        printf(" 1) Llenar carrito\n 2) Listar productos\n 3) Devolver producto\n 4) Pagar carrito\n 5) Salir\n");
+        printf("\nQue desea hacer: ");
+        scanf("%i", &opcion);
+        switch (opcion)
+        {
+        case 1:
+            llenarCarrito();
+            break;
+        case 2:
+            listarProductos();
+            break;
+        case 3:
+            devolverProducto();
+            break;
+        case 4:
+            pagarCarrito();
+            break;
+        case 5:
+            printf("\nBye\n");
+            break;
+        default:
+            printf("\nIngresa una opcion entre 1 y 5\n");
+            break;
+        }
+    }
     return 0;
 }
