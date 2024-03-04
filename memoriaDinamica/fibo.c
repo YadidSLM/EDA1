1f69b41fb814a0c5897fc5d41412d3b3cd869f3f
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2024-03-03 23:14:01 -0600

    Programa que mide el tiempo de ejecución parcialmente ya que imprimer en ocaciones tiempos negativos

--- memoriaDinamica/fibo.c
+++ memoriaDinamica/fibo.c
@@ -1,29 +1,47 @@
+#define _POSIX_C_SOURCE 199309L
 #include <stdio.h>
 #include <stdlib.h>
-//Por referencia
-void fibonacci (int *a, int n)
+#include <time.h>
+
+long long current_timestamp()
 {
-    *a = 1;
-    *(a + 1) = 1;
-    for (int i = 0; i < n; i++)
-    {
-        if(i < n - 2)
-            *(a + i + 2) = *(a + i) + *((a + i) + 1);
-        printf("%i, ", *(a + i));
-    }
+    struct timespec te;
+    clock_gettime(CLOCK_REALTIME, &te);
+    return te.tv_nsec;
 }
+
+//Por referencia
+void fibonacci (unsigned long long int *a, int n)
+{
+  *a = 1;
+  *(a + 1) = 1;
+  for (unsigned long long int i = 0; i < n; i++)
+  {
+    if(i < n - 2)
+      *(a + i + 2) = *(a + i) + *((a + i) + 1);
+    printf("%llu, ", *(a + i));
+  }
+}
+
+
 
 int main(void)
 {
+    
+    long long begin = current_timestamp();
+
     int elementos;
-    system("cls");
     printf("\n\nFibonacci\n\n");
     printf("Ingresa el numero del elemento de la serie de fibonacci a mostrar: ");
     scanf("%i", &elementos);
-    //*apFib = fib es lo mismo que *apFib = &fib[0]
-    int fib[elementos];
+    unsigned long long int fib[elementos], *apFib = &fib[0];
     //Por referencia
-    fibonacci(&fib[0], elementos);
-    printf("\nValor del elemento ingresado: %i\n", fib[elementos - 1]);
+    fibonacci(apFib, elementos);
+    printf("\nValor del elemento ingresado: %llu\n", fib[elementos - 1]);
+
+    long long end = current_timestamp();
+    double timeProceso = (end - begin) / 1000000000.0; // Convertir de nanosegundos a segundos
+    printf("Tiempo que tarda la ejecucion: %lf\n", timeProceso);
+
     return 0;
 }
