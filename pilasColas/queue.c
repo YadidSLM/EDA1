d6363b0afa8600f331cf269c9a0f261e009830c2
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2024-03-13 20:17:27 -0600

    Esructura inical

--- pilasColas/queue.c
+++ pilasColas/queue.c
@@ -1,3 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct queue{
+    int head, tail, size;
+    unsigned capacity;
+    int *array;
+};
+
+struct queue* createCola(int capacity)
+{
+    
+}
+
+int main ()
+{
+    struct queue* clientes = crearCola(20);
+    return 0;
+}
