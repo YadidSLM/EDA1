91c3dcd3affcde739c9259321066cf779fc2e1ea
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2024-03-13 21:49:57 -0600

    Haciendo pruebas no quita el dequeue

--- pilasColas/queue.c
+++ pilasColas/queue.c
@@ -41,15 +41,15 @@ void enqueue(struct queue* cola, int clienteNum)
 
 int dequeue (struct queue* cola)
 {
-    int clienteAtendido;
+    int cli = 0;
     if(isEmpty(cola))
         printf("\nLa cola esta vacia, no se pueden quitar elementos que no hay.\n");
     else{
-        clienteAtendido = cola->array[cola->head];
-        cola->head++;
-        cola->size--;
+        cli = cola->array[cola->head];
+        cola->head = (cola->head + 1) % cola->capacity;
+        cola->size = cola->size - 1;
+        return cli;
     }
-    return clienteAtendido;
 }
 
 int peek(struct queue* cola)
@@ -60,7 +60,14 @@ int peek(struct queue* cola)
 int main ()
 {
     struct queue* clientes = createCola(20);
+    int clienteAtendido;
     enqueue(clientes, 5);
+    enqueue(clientes, 2);
+    enqueue(clientes, 4);
+    clienteAtendido = dequeue(clientes);
+    printf("\nCliente atendido: %i",clienteAtendido);
+    printf("\nCLiente a ser atendido: %i", peek(clientes));
+    printf("\nSize: %i, Tail: %i, Head: %i, Elemento en head: %i\n", clientes->size, clientes->tail, clientes->head, clientes->array[clientes->head]);
 
     return 0;
 }
