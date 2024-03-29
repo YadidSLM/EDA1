05fad78dd77179f7c1c8477353505f8a6cb8c753
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2024-03-28 22:08:10 -0600

    Avance de josefo listo, aún no concluido

--- colaCircular/josefo.c
+++ colaCircular/josefo.c
@@ -67,12 +67,58 @@ void show (struct colaCir* p)
         printf("Cola circular vacia. \n");
         printf("Head: %i, Tail, %i, Size: %i, Capacidad max: %i\n", p->head, p->tail, p->size, p->capacity);
     }
-    
+
 }
+
+int peekIn(struct colaCir* p, int n, int max)
+{
+    if(n >= 0 && n < max)
+        return p->array[n];
+    else
+        return 0;
+}
+
+void josefo(struct colaCir* p, int n, int max)
+{
+    int eliminadosT = (max - n + 1), sigE = n - 1 + n, anadirEx = 0, nSobrevivientes = n - 1;
+    int orden[eliminadosT];
+    printf("\neliminadosT: %d, sigueE: %d, sobr: %d\n", eliminadosT, sigE, nSobrevivientes);
+    if(n > max)
+        printf("Nadie es eliminado porque no existe la persona en la posiciòn %d", n);
+    if(n == 1)
+        show(p); // Todos son eliminados, se muestra el total de los elementos de la cola
+    if(n > 1 && n <= max)
+    {
+        for(int i = 0; i < eliminadosT; i++)
+        {
+            if(i == 0)
+                orden[i] = peekIn(p, n - 1, max); //Primer elemento eliminado
+            if(i > 0){
+                if((sigE) > max){
+                    anadirEx = sigE - max;
+                    n = 0;
+                    n + anadirEx;
+                    orden[i] = peekIn(p, n, max);
+                }
+                orden[i] = peekIn(p, n, max);
+                printf("%d, ", orden[i]);
+            }
+            //printf("%d, ", orden[i]);
+            printf("\norden 0: %d, eliminadosT: %d, i: %d, sigueE: %d, sobr: %d", orden[1], eliminadosT, i, sigE, nSobrevivientes);
+        }
+    }
+
+}
+
 // int main(int argc, char const *argv[]){
 int main(int argc, char const *argv[])
 {
-    struct colaCir* personas = crearCirQueue(argv[1]); //Máximo de personas en primer
+    int max = 8, n = 5;
+    struct colaCir* personas = crearCirQueue(max); //Máximo de personas en primer
     //cambiar de cadena a numero con atoi o con otra función o varios if
+    for(int i = 1; i <= max; i++)
+        enqueue(personas, i);
+    show(personas);
+    josefo(personas, n, max);
     return 0;
 }
