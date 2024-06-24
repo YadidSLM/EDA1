69a0b9059b038e883f81147164b348ef5af1c7ef
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2024-06-23 21:09:50 -0600

    Se realizó un programa de cocktailsort

--- sorts/cocktail_sort/cocktail_sort.py
+++ sorts/cocktail_sort/cocktail_sort.py
@@ -1,15 +1,12 @@
 import random
-i_times = 0
 def cocktailSort(arr):
     global i_times
     for a in range(len(arr) // 2):
         print(a)
         for i in range(a, len(arr) - 1):
-            i_times += 1
             if arr[i] > arr[i + 1]:
                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
         for i in range(len(arr) - (a + 1), 0, -1):
-            i_times += 1
             if arr[i] < arr[i - 1]:
                 arr[i], arr[i - 1] = arr[i - 1], arr[i]
     print(arr)
@@ -20,4 +17,3 @@ random.shuffle(arr)
 print(f'{arr}\n')
 sorted = cocktailSort(arr)
 print(sorted)
-print(i_times)
